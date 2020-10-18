af603e303964742e5b34a3c79420f7813823ec2b
Sean Anih <sean.anih@gmail.com>
2020-10-18 02:08:58 -0400

    initial commit. basic app structure complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1>My To-Do List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
