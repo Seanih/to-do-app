6a4d2dcd9c246f9cf72ac742d01fc5c16496e579
Sean Anih <sean.anih@gmail.com>
2020-10-18 12:45:49 -0400

    finished with basic functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
+import ToDoList from './components/ToDoList';
 
 const App = () => {
+
+  const [inputText, setInputText] = useState('')
+  const [toDos, setToDos] = useState([])
+
   return (
     <div className="App">
       <header>
         <h1>My To-Do List</h1>
       </header>
-      <Form />
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        toDos={toDos}
+        setToDos={setToDos}
+      />
+      <ToDoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,48 @@
 import React from 'react'
 
-const Form = () => {
+const Form = ({ inputText, setInputText, toDos, setToDos }) => {
+
+  const inputTextHandler = e => {
+    console.log(e.target.value)
+    setInputText(e.target.value)
+  };
+
+  const submitToDoHandler = e => {
+    e.preventDefault();
+    setToDos([
+      ...toDos,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.floor(Math.random() * 100)
+      }
+    ])
+    setInputText('')
+  }
+
   return (
-    <>
-      <form>
-        <input type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
-          <i className="fas fa-plus-square"></i>
-        </button>
-        <div className="select">
-          <select name="todos" className="filter-todo">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="uncompleted">Uncompleted</option>
-          </select>
-        </div>
-      </form>
-    </>
+    <form>
+      <input
+        type="text"
+        className="todo-input"
+        onChange={inputTextHandler}
+        value={inputText}
+      />
+      <button
+        className="todo-button"
+        type="submit"
+        onClick={submitToDoHandler}
+      >
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
   )
 }
 
